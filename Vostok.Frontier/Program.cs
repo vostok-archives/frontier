8d3a53790086449822fe87777c58dcd2eae63ed2
lapin <lapin@kontur.ru>
2018-01-29 14:06:37 +0400

    cleanup code

--- Vostok.Frontier/Program.cs
+++ Vostok.Frontier/Program.cs
@@ -9,28 +9,6 @@ namespace Vostok.Frontier
 {
     public class Program
     {
-        //public static void Main(string[] args)
-        //{
-        //    BuildWebHost(args).Run();
-        //}
-
-        //private static IWebHost BuildWebHost(string[] args)
-        //{
-        //    return WebHost.CreateDefaultBuilder(args)
-        //        .UseKestrel()
-        //        .ConfigureAppConfiguration((hostingContext, config) =>
-        //        {
-        //            config.AddJsonFile("appsettings.json", false, true);
-        //        })
-        //        .UseUrls("http://+:6302/")
-        //        .AddVostokServices()
-        //        //.UseVostokConfigureAirlock()
-        //        //.ConfigureVostokMetrics()
-        //        //.ConfigureVostokLogging()
-        //        .UseStartup<Startup>()
-        //        .Build();
-        //}
-
         public static void Main(string[] args)
         {
             BuildVostokHost(args).Run();
